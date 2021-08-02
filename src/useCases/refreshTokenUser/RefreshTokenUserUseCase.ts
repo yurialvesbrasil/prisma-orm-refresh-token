02b4202d137229a79c29ce262c199bff9ebd56b5
Yuri Brasil <yurialvesbrasil@gmail.com>
2021-08-02 19:56:17 -0300

    Verifica a expiração do refresh token

--- src/useCases/refreshTokenUser/RefreshTokenUserUseCase.ts
+++ src/useCases/refreshTokenUser/RefreshTokenUserUseCase.ts
@@ -1,7 +1,7 @@
 import { prismaClient } from '@src/prisma/client';
 import AuthService from '@src/services/AuthService';
 import { ServiceInternalError } from '@src/util/errors/api-error';
-
+import dayjs from 'dayjs';
 export class RefreshTokenUserUseCase {
   async refreshToken(refresh_token: string) {
     const refreshTokenObject = await prismaClient.refreshToken.findFirst({
@@ -18,6 +18,14 @@ export class RefreshTokenUserUseCase {
         'Refresh token invalid',
         'The Refresh Token informed was not found in the records.'
       );
+
+    /* Verifica se o token está expirado */
+    const refreshTokenExpired = dayjs().isAfter(dayjs.unix(refreshTokenObject.expiresIn));
+    if (!refreshTokenExpired)
+    throw new ServiceInternalError(
+      'Refresh Token Expired',
+      'Refresh your login.'
+    );
 
     const token = AuthService.generateToken(
       refreshTokenObject.userId,
